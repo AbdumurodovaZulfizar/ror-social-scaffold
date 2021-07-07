5617d15e9cda8e064b29255d954e653b7977746b
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-07-07 09:32:53 +0500

    Modify user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe User, type: :model do
 
     it { is_expected.to have_many(:comments) }
     it { is_expected.to have_many(:likes) }
+    it { is_expected.to have_many(:friendships) }
+    it { is_expected.to have_many(:request_received) }
   end
 
   describe 'validations' do
