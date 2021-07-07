bdc160c11e604af358ffb4cef547ae78068b563a
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-07-07 09:47:58 +0500

    Fix linter errors

--- spec/factories/friendship.rb
+++ spec/factories/friendship.rb
@@ -1,7 +1,7 @@
-# rubocop:disable Layout/EndOfLine, Lint/EmptyBlock
+# rubocop:disable Layout/EndOfLine
 FactoryBot.define do
   factory :friendship do
     user {}
   end
 end
-# rubocop:enable Layout/EndOfLine, Lint/EmptyBlock
+# rubocop:enable Layout/EndOfLine
