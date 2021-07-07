86abb511f20142e63c575a193b2ad6b48f4d268c
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-07-07 21:54:17 +0500

    Modify user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
+    @user_friend_posts = @user.posts.include?(@friend)
   end
 end
